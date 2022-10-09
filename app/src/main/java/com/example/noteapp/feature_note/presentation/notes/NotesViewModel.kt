db31326a72330b3eeaa7123cba61cd2efd28576a
Juliantoferdian24 <Juliantoferdian24@gmail.com>
2022-10-09 14:55:46 +0700

    feat: Update ViewModel When NotesEvent.Order

--- app/src/main/java/com/example/noteapp/feature_note/presentation/notes/NotesViewModel.kt
+++ app/src/main/java/com/example/noteapp/feature_note/presentation/notes/NotesViewModel.kt
@@ -6,7 +6,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.noteapp.feature_note.domain.model.Note
 import com.example.noteapp.feature_note.domain.use_case.NoteUseCases
+import com.example.noteapp.feature_note.domain.util.NoteOrder
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -20,10 +24,17 @@ class NotesViewModel @Inject constructor(
 
     private var recentlyDeletedNote: Note? = null
 
+    private var getNotesJob: Job? = null
+
     fun onEvent(event: NotesEvent) {
         when (event) {
             is NotesEvent.Order -> {
-
+                if (state.value.noteOrder::class == event.noteOrder::class &&
+                    state.value.noteOrder.orderType == event.noteOrder.orderType
+                ) {
+                    return
+                }
+                getNotes(event.noteOrder)
             }
             is NotesEvent.DeleteNote -> {
                 viewModelScope.launch {
@@ -43,5 +54,17 @@ class NotesViewModel @Inject constructor(
                 )
             }
         }
+    }
+
+    private fun getNotes(noteOrder: NoteOrder) {
+        getNotesJob?.cancel()
+        getNotesJob = noteUseCases.getNotes(noteOrder)
+            .onEach { notes ->
+                _state.value = state.value.copy(
+                    notes = notes,
+                    noteOrder = noteOrder
+                )
+            }
+            .launchIn(viewModelScope)
     }
 }
