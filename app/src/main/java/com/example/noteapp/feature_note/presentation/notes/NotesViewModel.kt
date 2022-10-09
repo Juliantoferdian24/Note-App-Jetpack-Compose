e8c213bf5fe70a52f9f930ab22f45a40f4f032ea
Juliantoferdian24 <Juliantoferdian24@gmail.com>
2022-10-09 14:28:57 +0700

    feat: Update ViewModel When NotesEvent.DeleteNote

--- app/src/main/java/com/example/noteapp/feature_note/presentation/notes/NotesViewModel.kt
+++ app/src/main/java/com/example/noteapp/feature_note/presentation/notes/NotesViewModel.kt
@@ -1,8 +1,12 @@
 package com.example.noteapp.feature_note.presentation.notes
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.noteapp.feature_note.domain.use_case.NoteUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,4 +14,25 @@ class NotesViewModel @Inject constructor(
     private val noteUseCases: NoteUseCases
 ) : ViewModel() {
 
+    private val _state = mutableStateOf(NotesState())
+    val state: State<NotesState> = _state
+
+    fun onEvent(event: NotesEvent) {
+        when (event) {
+            is NotesEvent.Order -> {
+
+            }
+            is NotesEvent.DeleteNote -> {
+                viewModelScope.launch {
+                    noteUseCases.deleteNotes(event.note)
+                }
+            }
+            is NotesEvent.RestoreNote -> {
+
+            }
+            is NotesEvent.ToggleOrderSection -> {
+
+            }
+        }
+    }
 }
